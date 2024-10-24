ed9987ad05c2b97490e70daa3b636096ee1e5778
ZosiscoIV <145712189+ZosiscoIV@users.noreply.github.com>
2024-10-24 12:16:04 +0200

    Update temps.h

--- temps.h
+++ temps.h
@@ -3,14 +3,19 @@
 #include <time.h>
 #include <unistd.h>
 
+#define SECTEUR1 25
+#define SECTEUR2 20
+#define SECTEUR3 30
+
 float temps_secteur(float baseTime) {
   int i;
   srand(time(NULL)); // Utilise comme seed de création de nombre aléatoire le temps en seconde depuis janvier 1970
   float nombre_aleatoire = rand() % 3000 + 1;  // Génère un nombre pseudo-aléatoire
-  temps_S = nombre_aléatoire/1000 + baseTime;
-  return temps_S
+  float temps_S = nombre_aleatoire/1000 + baseTime;
+  return temps_S;
 }
 
-int main() {
-  return 0;
+
+void afficher_temps(float temps, int num_pilote) {
+  printf("Pilote n°%d ---> %.3f sec\n", num_pilote, temps);
 }
