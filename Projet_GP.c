3f239b175c96dc9a98c42f9d248c5e11f088b052
ZosiscoIV <145712189+ZosiscoIV@users.noreply.github.com>
2024-10-31 10:46:26 +0100

    Update Projet_GP.c
    Ajouter de quoi gérer les temps mais manque mémoire partagée pour que ça marche

--- Projet_GP.c
+++ Projet_GP.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include "temps.h"
 
+int num_pilotes[20] = {1, 11, 44, 63, 16, 55, 4, 81, 14, 18, 10, 31, 23, 2, 22, 3, 77, 24, 20, 27};
+
 void voiture(int i){
-  int num_pilotes[20] = {1, 11, 44, 63, 16, 55, 4, 81, 14, 18, 10, 31, 23, 2, 22, 3, 77, 24, 20, 27};
   float s1 = temps_secteur(SECTEUR1);
   afficher_temps(s1, num_pilotes[i]);
+  stocker_temps(s1, num_pilotes[i], i, 1);
+
+  float s2 = temps_secteur(SECTEUR2);
+  afficher_temps(s2, num_pilotes[i]);
+  stocker_temps(s2, num_pilotes[i], i, 2);
+
+  float s3 = temps_secteur(SECTEUR3);
+  afficher_temps(s3, num_pilotes[i]);
+  stocker_temps(s3, num_pilotes[i], i, 3);
 }
 
 
@@ -15,8 +26,9 @@ void voiture(int i){
 int main() {
   int child_pid;
   int i;
+  int size_pilotes = sizeof(num_pilotes)/sizeof(num_pilotes[0]);
 
-  for(i = 0; i < 20; i++) {
+  for(i = 0; i < size_pilotes; i++) {
     child_pid = fork();
   
     if(child_pid == -1) {
@@ -31,10 +43,12 @@ int main() {
   }
   
   // Processus parent attend que tous les enfants se terminent
-  for (i = 0; i < 20; i++) {
+  for (i = 0; i < size_pilotes; i++) {
       wait(NULL);  // Attend chaque processus enfant pour éviter les zombies
   }
-  
+  for (i = 0; i < size_pilotes; i++) {
+    printf("%.3f ", temps_s1[i]);
+  }
 
   return 0;
 }
