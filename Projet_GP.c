894661c9e20c0f67c873f6ec4085fe9fba91f5c6
ZosiscoIV <145712189+ZosiscoIV@users.noreply.github.com>
2024-11-07 13:17:34 +0100

    Update Projet_GP.c

--- Projet_GP.c
+++ Projet_GP.c
@@ -8,17 +8,31 @@
 int num_pilotes[20] = {1, 11, 44, 63, 16, 55, 4, 81, 14, 18, 10, 31, 23, 2, 22, 3, 77, 24, 20, 27};
 
 void voiture(int i){
+
+  // ouverture d'un fichier pour stocker les temps
+  int fichier = open("temps_stockage.txt", O_CREAT|O_APPEND|O_RDWR, 0666);
+  if (fichier == -1) {
+    perror("Erreur lors de l'ouverture du fichier temps_stockage");
+    exit(EXIT_FAILURE);
+  }
+  else {
+    close(fichier);
+  }
+
+
   float s1 = temps_secteur(SECTEUR1);
   afficher_temps(s1, num_pilotes[i]);
-  stocker_temps(s1, num_pilotes[i], i, 1);
+  stocker_temps(s1, num_pilotes[i], i, 1, fichier);
 
   float s2 = temps_secteur(SECTEUR2);
   afficher_temps(s2, num_pilotes[i]);
-  stocker_temps(s2, num_pilotes[i], i, 2);
+  stocker_temps(s2, num_pilotes[i], i, 2, fichier);
 
   float s3 = temps_secteur(SECTEUR3);
   afficher_temps(s3, num_pilotes[i]);
-  stocker_temps(s3, num_pilotes[i], i, 3);
+  stocker_temps(s3, num_pilotes[i], i, 3, fichier);
+
+  close(fichier); //fermeture après utilisation
 }
 
 
@@ -27,6 +41,7 @@ int main() {
   int child_pid;
   int i;
   int size_pilotes = sizeof(num_pilotes)/sizeof(num_pilotes[0]);
+  
 
   for(i = 0; i < size_pilotes; i++) {
     child_pid = fork();
@@ -38,7 +53,7 @@ int main() {
   
     if(child_pid == 0){
       voiture(i);
-      exit(0);  // Le processus enfant se termine après avoir simulé la voiture
+      exit(EXIT_SUCCESS);  // Le processus enfant se termine après avoir simulé la voiture
     }
   }
   
@@ -47,7 +62,7 @@ int main() {
       wait(NULL);  // Attend chaque processus enfant pour éviter les zombies
   }
   for (i = 0; i < size_pilotes; i++) {
-    printf("%.3f ", temps_s1[i]);
+    afficher_temps(temps_totaux[i], num_pilotes[i]);
   }
 
   return 0;
