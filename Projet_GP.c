6336a36bb376f78a90cc3bb96b4bafe52fc9bbbd
ZosiscoIV <145712189+ZosiscoIV@users.noreply.github.com>
2024-12-29 21:46:48 +0100

    Update Projet_GP.c
    il manque juste le passage des résultats de q1 à q2 et de q2 à q3

--- Projet_GP.c
+++ Projet_GP.c
@@ -8,8 +8,11 @@
 #include <string.h>
 #include "temps.h"
 
+#define NOMBRE_PILOTES_BASE 20
+#define NOMBRE_PILOTES_Q2 15
+#define NOMBRE_PILOTES_Q3 10
 
-int num_pilotes[20] = {1, 11, 44, 63, 16, 55, 4, 81, 14, 18, 10, 31, 23, 2, 22, 3, 77, 24, 20, 27};
+int num_pilotes[NOMBRE_PILOTES_BASE] = {1, 11, 44, 63, 16, 55, 4, 81, 14, 18, 10, 31, 23, 2, 22, 3, 77, 24, 20, 27};
 
 
 
@@ -29,7 +32,7 @@ void BubbleSort(voit a[], int array_size){
 
 
 
-void course(int i, bool isQualif){
+void course(int i, bool isQualif, int participants){
 
   // Ouverture d'un fichier pour stocker les temps
   int fichier = open("temps_stockage.txt", O_CREAT|O_APPEND|O_RDWR, 0666);
@@ -45,7 +48,7 @@ void course(int i, bool isQualif){
 
   float s1 = temps_secteur(SECTEUR1, 2);
   //afficher_temps(s1, num_pilotes[i]);
-  stocker_temps(s1, num_pilotes[i], i, 1, fichier);
+  //stocker_temps(s1, num_pilotes[i], i, 1, fichier);
 
   float s2 = temps_secteur(SECTEUR2, 4);
   //afficher_temps(s2, num_pilotes[i]);
@@ -55,17 +58,25 @@ void course(int i, bool isQualif){
   //afficher_temps(s3, num_pilotes[i]);
   //stocker_temps(s3, num_pilotes[i], i, 3, fichier);
 
-  srand(i);
-  float test = rand() % 21;
-  bool pit = (rand() % 20 == 0);
-  bool out = (rand() % 100 == 0);
+  int pid = getpid();
+  srand(i + pid + time(NULL));
+  bool pit = (rand() % 6 == 0);
+  bool v_out = (rand() % 40 == 0);
 
-
+  if (pit) {
+    s3 = s3 + 25;
+  }
   float tour = s1 + s2 + s3;
 
-  int shared_memory = shmget(69, 20*sizeof(voit), 0666);
+  int shared_memory = shmget(6969, participants*sizeof(voit), 0666);
   voit *tab_voit = shmat(shared_memory, 0, 0666);
   
+  
+
+  if (tab_voit[i].out) {
+    v_out = true;
+  }
+
   float stock_total = tab_voit[i].temps_total;
   if (!isQualif) {
     stock_total = stock_total + tour;
@@ -74,10 +85,12 @@ void course(int i, bool isQualif){
       stock_total = tour;
   }
 
-
-  voit temporary_voit = {num, s1, s2, s3, tour, stock_total, pit, out};
-
-  tab_voit[i] = temporary_voit;
+  if (!v_out || tab_voit[i].num_p == 0) {
+    voit temporary_voit = {num, s1, s2, s3, tour, stock_total, pit, v_out};
+    tab_voit[i] = temporary_voit;
+  } else {
+    tab_voit[i].out = v_out;
+  }
 
   //afficher_temps(total, num_pilotes[i]);
 
@@ -87,13 +100,13 @@ void course(int i, bool isQualif){
 }
 
 
-bestTimes lecture_tri_affichage(voit *tableau_voit, int size_pilotes, bestTimes best_times) {
+bestTimes lecture_tri_affichage(voit *tableau_voit, int size_pilotes, bestTimes best_times, int participants) {
   int i;
-  voit lecture_voit[20]; // Crée un tableau pour stocker tout les temps
+  voit lecture_voit[participants]; // Crée un tableau pour stocker tout les temps
 
-  memcpy(lecture_voit, tableau_voit, 20*sizeof(voit)); // Fait une copie de la mémoire partagée
+  memcpy(lecture_voit, tableau_voit, participants*sizeof(voit)); // Fait une copie de la mémoire partagée
 
-  BubbleSort(lecture_voit, 20); // Trie le tableau des temps
+  BubbleSort(lecture_voit, participants); // Trie le tableau des temps
 
   printf("---------------------------------------------------------------------------\n");
   for (i = 0; i < size_pilotes; i++) {
@@ -110,10 +123,10 @@ bestTimes lecture_tri_affichage(voit *tableau_voit, int size_pilotes, bestTimes 
       best_times.best_tour = lecture_voit[i];
     }
     if (i == 0) {
-      afficher_temps(lecture_voit[i].temps_total, lecture_voit[i].num_p, lecture_voit[i].temps_total, lecture_voit[i].temps_tour);
+      afficher_temps(lecture_voit[i], lecture_voit[i]);
     }
     else {
-      afficher_temps(lecture_voit[i].temps_total, lecture_voit[i].num_p, lecture_voit[i-1].temps_total, lecture_voit[i].temps_tour);
+      afficher_temps(lecture_voit[i], lecture_voit[i-1]);
     }
   }
   printf("| s1 n°%2d : %6.3f, s2 n°%2d : %6.3f, s3 n°%2d : %6.3f, tour n°%2d : %6.3f |\n", best_times.best_s1.num_p, best_times.best_s1.temps_s1, best_times.best_s2.num_p, best_times.best_s2.temps_s2, best_times.best_s3.num_p, best_times.best_s3.temps_s3, best_times.best_tour.num_p, best_times.best_tour.temps_tour);
@@ -123,15 +136,14 @@ bestTimes lecture_tri_affichage(voit *tableau_voit, int size_pilotes, bestTimes 
 
 
 
-
-int grand_prix(int nbr_tours) {
+int grand_prix(int nbr_tours, bool qualif, int participants_course) {
   int child_pid;
   int h, i, j, k, l;
-  int size_pilotes = sizeof(num_pilotes)/sizeof(num_pilotes[0]);
+  int size_pilotes = participants_course; //sizeof(num_pilotes)/sizeof(num_pilotes[0]);
 
 
   // Création de la mémoire partagée
-  int shared_memory = shmget(69, 20*sizeof(voit), IPC_CREAT | 0666);
+  int shared_memory = shmget(6969, 20*sizeof(voit), IPC_CREAT | 0666);
   if (shared_memory == -1) {
     perror("Erreur lors de la création de la mémoire partagée");
     exit(EXIT_FAILURE);
@@ -145,9 +157,8 @@ int grand_prix(int nbr_tours) {
 
 
 
-
   // Initialisation de la mémoire partagée
-  for (i = 0; i < 20; i++) {
+  for (i = 0; i < participants_course; i++) {
     tab_voit[i] = (voit){0};
   }
 
@@ -165,7 +176,7 @@ int grand_prix(int nbr_tours) {
       }
   
       if(child_pid == 0){
-        course(i, false);
+        course(i, qualif, participants_course);
         exit(EXIT_SUCCESS);  // Le processus enfant se termine après avoir simulé la voiture
       }
     }
@@ -175,7 +186,7 @@ int grand_prix(int nbr_tours) {
         wait(NULL);  // Attend chaque processus enfant pour éviter les zombies
     }
     // Lecture et affichage des temps en mémoire partagée
-    best_times = lecture_tri_affichage(tab_voit, size_pilotes, best_times);
+    best_times = lecture_tri_affichage(tab_voit, size_pilotes, best_times, participants_course);
     sleep(1);
   }
 
@@ -195,90 +206,6 @@ int grand_prix(int nbr_tours) {
 
   return 0;
 }
-
-
-
-
-int q1(int nbr_tours) {
-  int child_pid;
-  int h, i, j, k, l;
-  int size_pilotes = sizeof(num_pilotes)/sizeof(num_pilotes[0]);
-
-
-  // Création de la mémoire partagée
-  int shared_memory = shmget(69, 20*sizeof(voit), IPC_CREAT | 0666);
-  if (shared_memory == -1) {
-    perror("Erreur lors de la création de la mémoire partagée");
-    exit(EXIT_FAILURE);
-  }
-  voit *tab_voit = shmat(shared_memory, 0, 0666);
-  if (tab_voit == (voit *)-1) {
-        perror("Erreur lors de l'attachement de la mémoire partagée");
-        exit(EXIT_FAILURE);
-  }
-
-
-
-
-
-  // Initialisation de la mémoire partagée
-  for (i = 0; i < 20; i++) {
-    tab_voit[i] = (voit){0};
-  }
-
-  bestTimes best_times = {0};  // Initialisation des meilleurs temps
-  best_times.best_s1.temps_s1 = best_times.best_s2.temps_s2 = best_times.best_s3.temps_s3 = best_times.best_tour.temps_tour = 1000;
-  // Simulation de plusieurs tours
-  for (h = 0; h < nbr_tours; h++) {
-    // Création des processus fils
-    for(i = 0; i < size_pilotes; i++) {
-      child_pid = fork();
-  
-      if(child_pid == -1) {
-        perror("Erreur lors du fork");
-        exit(EXIT_FAILURE);
-      }
-  
-      if(child_pid == 0){
-        course(i, true);
-        exit(EXIT_SUCCESS);  // Le processus enfant se termine après avoir simulé la voiture
-      }
-    }
-    
-    // Processus parent attend que tous les enfants se terminent
-    for (l = 0; l < size_pilotes; l++) {
-        wait(NULL);  // Attend chaque processus enfant pour éviter les zombies
-    }
-    // Lecture et affichage des temps en mémoire partagée
-    best_times = lecture_tri_affichage(tab_voit, size_pilotes, best_times);
-    sleep(1);
-  }
-
-
-
-
-  // Détacher la mémoire partagée
-  if (shmdt(tab_voit) == -1) {
-    perror("Erreur lors du détachement de la mémoire partagée");
-    exit(EXIT_FAILURE);
-  }
-  if (shmctl(shared_memory, IPC_RMID, NULL) == -1) {
-    perror("Erreur lors de la suppression de la mémoire partagée");
-    exit(EXIT_FAILURE);
-  }
-
-
-  return 0;
-}
-
-void q2(int nbr_tours){
-  printf("c'est ok q2!\n");
-}
-
-void q3(int nbr_tours){
-  printf("c'est ok q3!\n");
-}
-
 
 
 
@@ -286,21 +213,21 @@ int main(int argc, char **argv) {
   int tours = atoi(argv[2]);
   if (argc > 1) {
       if (strcmp(argv[1], "gp") == 0) {
-          grand_prix(tours);
+          grand_prix(tours, false, NOMBRE_PILOTES_BASE);
       } else if (strcmp(argv[1], "q1") == 0) {
-          q1(tours);
+          grand_prix(tours, true, NOMBRE_PILOTES_BASE);
       } else if (strcmp(argv[1], "q2") == 0) {
-          q2(tours);
+          grand_prix(tours, true, NOMBRE_PILOTES_Q2);
       } else if (strcmp(argv[1], "q3") == 0) {
-          q3(tours);
+          grand_prix(tours, true, NOMBRE_PILOTES_Q3);
       } else if (strcmp(argv[1], "p1") == 0) {
-          q1(tours);
+          grand_prix(tours, true, NOMBRE_PILOTES_BASE);
       } else if (strcmp(argv[1], "p2") == 0) {
-          q1(tours);
+          grand_prix(tours, true, NOMBRE_PILOTES_BASE);
       } else if (strcmp(argv[1], "p3") == 0) {
-          q1(tours);
+          grand_prix(tours, true, NOMBRE_PILOTES_BASE);
       } else if (strcmp(argv[1], "sprint") == 0) {
-          grand_prix(tours);
+          grand_prix(tours, false, NOMBRE_PILOTES_BASE);
       } else {
           printf("Veuillez fournir un argument valide (ex: grand_prix, q1, ...).\n");
       }
